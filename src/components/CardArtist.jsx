7d1b36b5dc5d8c03307f1b73fb5591476db3e53c
thomas <thomas.chalanson@sfr.fr>
2022-03-03 14:20:49 +0100

    style the cards artist and vinyl

--- src/components/CardArtist.jsx
+++ src/components/CardArtist.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 
-const CardArtist = ({ image, artist_name, techno, vinyl, id }) => {
+const CardArtist = ({
+  image,
+  artist_name,
+  techno,
+  vinyl,
+  country,
+  style,
+  id,
+}) => {
   return (
-    <div className="max-w-sm overflow-hidden rounded shadow-lg">
-      <img className="w-full" src={image} alt={artist_name} />
+    <div className="max-w-xs overflow-hidden rounded shadow-lg">
+      <div className="flex justify-center">
+        <img src={image} alt={artist_name} />
+      </div>
+
       <div className="px-6 py-4">
         <div className="mb-2 text-xl font-bold">{artist_name}</div>
       </div>
+
       <div className="px-6 pt-4 pb-2">
         <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
-          #{vinyl}
+          #
+          {style.slice(0, 1).map((data, i) => (
+            <div key={i}>{data}</div>
+          ))}
         </span>
         <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
-          #{techno}
+          #{country}
         </span>
       </div>
       {/* <div>
--- src/components/CardVinyl.jsx
+++ src/components/CardVinyl.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 
-const CardVinyl = ({ artist, image_vinyl, style,titles, vinyl_name, id }) => {
+const CardVinyl = ({ artist, image_vinyl, style, titles, vinyl_name, id }) => {
   return (
-    <div className="max-w-sm overflow-hidden rounded shadow-lg">
+    <div className="max-w-xs overflow-hidden rounded shadow-lg">
       <img className="w-full" src={image_vinyl} alt={vinyl_name} />
       <div className="px-6 py-4">
         <div className="mb-2 text-xl font-bold">{vinyl_name}</div>
       </div>
+      <div className="px-6 py-4">
+        <div className="mb-2 text-xl font-bold text-green-800">{artist}</div>
+      </div>
       <div className="px-6 pt-4 pb-2">
         <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
-          #{style}
+          {style.slice(0, 1).map((data, i) => (
+            <div key={i}>{data}</div>
+          ))}
         </span>
         <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
-          #{titles}
+          {style.slice(1, 2).map((data, i) => (
+            <div key={i}>{data}</div>
+          ))}
         </span>
       </div>
       {/* <div>
