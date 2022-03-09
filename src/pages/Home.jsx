b095aacd9a4b3742f7d112ee4b1dbf61a3b708f9
thomas <thomas.chalanson@sfr.fr>
2022-03-09 17:47:01 +0100

    create modals

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import SignInModal from "../components/Modals/SignInModal";
+import SignUpModal from "../components/Modals/SignUpModal";
 import Upload from "../Upload";
 
 function Home() {
@@ -7,7 +9,8 @@ function Home() {
       <h1 className="my-10 text-3xl mt-15 m text-emerald-700">
         Teknoland Production
       </h1>
-
+      <SignUpModal />
+      <SignInModal />
       <Upload />
     </div>
   );
